6c48de7ff39c3d4f82603623121786b7614780b7
ullas <ullaskunder3@gmail.com>
2021-09-25 13:17:05 +0530

    PropTypes

--- src/DeveloperList.js
+++ src/DeveloperList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 // stateless functional component
 function DeveloperList(props) {
@@ -30,46 +31,10 @@ function DeveloperList(props) {
     );
 }
 
-// class DeveloperList extends React.Component{
-//     render(){
-//         // this.propes is an object contains arry property
-//         console.log(this.props);
-//         return(
-//             //
-//             <ol className="developer-list">
-//                 {/* object like syntax inside that we will have expression */}
-//             {
-//                 this.props.developerlist.map((developer) => (
-//                     <li key={ developer.id }
-//                         className="developer-item">
-
-//                         <div className= "developer-avatar"
-//                             style={ { backgroundImage: `url(${developer.avatarUrl})`} }
-//                              />
-
-//                         <div className="developer-details">
-//                             <span>{developer.id}</span>
-//                             <p>{ developer.name }</p>
-//                             <p>{ developer.email }</p>
-//                         </div>
-
-//                         <button className="contact-remove"></button>
-//                     </li>
-//                 ))
-//             }
-//         </ol>
-//         )
-//     }
-// }
+DeveloperList.propTypes = {
+    developerlist: PropTypes.array.isRequired,
+    onDeleteDev: PropTypes.func.isRequired
+}
 
 //exporting Component
 export default DeveloperList;
-
-// each child in an array or iterator should have unique "key" props.
-/**
- * The reason we need to add key = {} to each child is
- * eventually one of those list item may change
- * and by having unique key attribute on each list item
- * react will know which item in the list has changed
- * and recreate only that item rather than whole list item
- */
